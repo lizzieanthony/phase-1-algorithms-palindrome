5c842468e9bf69bde8366eb03c2e2e835e9eccb9
Lizzie Anthony <lizziewrab@me.com>
2021-11-19 14:30:58 -0600

    commit tree 48aaeef324eb50eacde2e36a8b260d193fb0fdbf

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    if (word.length <= 1) {
+      return true
+    } else {
+      if(word[0] === word[word.length -1]) {
+        return true 
+        isPalindrome(word.slice(1, -1))
+      } else {
+        return false
+      }
+    }
+    
 }
+  /*Write your algorithm here
+
+  
+  */ 
+
 
 /* 
   Add your pseudocode here
+
+   function isPalindrone(word) {
+    if the argument passed is a palindrome  return true 
+    else the word is not a palindrome return false
+  }
+
 */
 
 /*
   Add written explanation of your solution here
+
+  when a string is passed in the function read it forwards and backwards
+  make sure the letters are the same both ways 
+  if it matches return true
+  if not return false 
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +47,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome('mom'));
 }
 
 module.exports = isPalindrome;
